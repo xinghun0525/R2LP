fff0471cc7868aee1c0af71b29ec50fcee2e7554
Skyiop <129600605+Skyiop@users.noreply.github.com>
2023-09-17 23:33:31 +0800

    Update check_basic_topk_medium.cpp

--- R2LP_exp/full_bash/topk_query/check_basic_topk_medium.cpp
+++ R2LP_exp/full_bash/topk_query/check_basic_topk_medium.cpp
@@ -57,7 +57,7 @@ int main(int argc, char **argv) {
 	cout << "filename = " << filename << ", topk = " << topk << endl;
 	unordered_set<int> ground_idx;
 	ifstream fin;
-	string idx_path = "../ground_idx/" + filename + "/idx1";
+	string idx_path = "../../../ground_idx/" + filename + "/idx1";
 	cout << "idx_path = " << idx_path << endl;
 	fin.open(idx_path.data());
 	assert(fin.is_open());
