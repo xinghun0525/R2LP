4923794a7478672f9e6497503aca7a29f23dd815
Skyiop <129600605+Skyiop@users.noreply.github.com>
2023-09-17 14:00:51 +0800

    Add files via upload

---
+++ r2lp_exp/eps_query_large_graph_Table4_w.sh
@@ -0,0 +1,14 @@
+cd code
+rm main_eps_large_graph_w_pruning
+make main_eps_large_graph_w_pruning
+fn=("CP LJ wiki_zh")
+#fn=("LJ")
+eps=("0.01")
+for i in ${fn[*]}; do
+    mkdir -p ../rbslog/eps_query/$i
+    for k in ${eps[*]}; do
+        echo "$i $k start!!"
+        ./main_eps_large_graph_w_pruning $i $k 2>&1 > ../rbslog/eps_query/$i/eps-$k.log
+        echo "$i $k finished!!"
+    done
+done
---
+++ r2lp_exp/eps_query_small_graph_Table4_wo.sh
@@ -0,0 +1,13 @@
+cd code
+rm  main_eps_small_graph_wo_pruning
+make main_eps_small_graph_wo_pruning
+fn=("CG WV EN SD DB wiki_fy ND")
+eps=("0.01")
+for i in ${fn[*]}; do
+    mkdir -p ../rbslog/eps_query/$i
+    for k in ${eps[*]}; do
+        echo "$i $k start!!"
+        ./main_eps_small_graph_wo_pruning $i $k 2>&1 > ../rbslog/eps_query/$i/eps-$k.log
+        echo "$i $k finished!!"
+    done
+done
